5fab05a3ac80a91a5cf6cc591557b08bb997e9c3
Mohammed Imran <mohammedimran86992@gmail.com>
2024-06-02 14:50:05 +0530

    refactor: Update page layout and metadata for Dinesh's Portfolio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,54 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+import LocalFont from "next/font/local";
 
 export const metadata: Metadata = {
 	title: "Dinesh's Portfolio",
 	description:
 		"Dinesh M is a Interior Designer with a focus on creating beautiful, functional spaces that inspire.",
+	openGraph: {
+		type: "website",
+		url: "https://mdinesh.in/",
+		title: "Dinesh's Portfolio",
+		description: "Dinesh's Portfolio Description",
+		siteName: "Dinesh's Portfolio",
+		images: [
+			{
+				url: "https://mdinesh.in/_next/image?url=%2Fdinesh.jpg&w=640&q=75",
+				width: 640,
+				height: 640,
+			},
+		],
+	},
+	keywords: ["interior designer", "portfolio", "dinesh m"],
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			"max-video-preview": -1,
+			"max-image-preview": "large",
+			"max-snippet": -1,
+		},
+	},
+	twitter: {
+		title: "M Dinesh",
+		card: "summary_large_image",
+	},
 };
+
+const calSans = LocalFont({
+	src: "../../public/fonts/CalSans-SemiBold.ttf",
+	variable: "--font-calsans",
+});
+const inter = Inter({
+	subsets: ["latin"],
+	variable: "--font-inter",
+});
 
 export default function RootLayout({
 	children,
@@ -16,8 +56,10 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
+		<html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+			<body className={inter.className}>
+				<main className="flex flex-col min-h-[100dvh]">{children}</main>
+			</body>
 		</html>
 	);
 }
