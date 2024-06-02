6ffbf211c12667382c99da84df60a5b86ebaf302
Mohammed Imran <mohammedimran86992@gmail.com>
2024-06-02 15:21:58 +0530

    feat: Enable React strict mode, compression, ESLint ignore during builds, etags, and production profiling

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	reactStrictMode: true,
+	compress: true,
+	eslint: { ignoreDuringBuilds: true },
+	generateEtags: true,
+	reactProductionProfiling: true,
+};
 
 export default nextConfig;
