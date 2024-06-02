b1308aba9e1dc9ecdf72e0c59a7204448e150aad
Mohammed Imran <mohammedimran86992@gmail.com>
2024-06-02 15:22:04 +0530

    refactor: Update page layout and remove unused components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
 import LocalFont from "next/font/local";
+import "./globals.css";
 
 export const metadata: Metadata = {
 	title: "Dinesh's Portfolio",
