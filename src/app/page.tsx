e17d9da74b2feb0ce5b374ae7e6e0d0ebaae76fa
Mohammed Imran <mohammedimran86992@gmail.com>
2024-06-02 15:24:58 +0530

    refactor: Update page layout to use list items for navigation links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,13 +13,14 @@ export default function Page() {
 			<nav className="my-16 animate-fade-in">
 				<ul className="flex items-center justify-center gap-4">
 					{navigation.map((item) => (
-						<Link
-							key={item.href}
-							href={item.href}
-							className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
-						>
-							{item.name}
-						</Link>
+						<li key={item.href}>
+							<Link
+								href={item.href}
+								className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+							>
+								{item.name}
+							</Link>
+						</li>
 					))}
 				</ul>
 			</nav>
