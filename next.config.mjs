12da966dedec971d655d7467539aefa8c2ecc7f1
Mohammed Imran <mohammedimran86992@gmail.com>
2024-10-12 14:00:24 +0530

    Refactor next.config.mjs and tsconfig.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	compress: true,
 	eslint: { ignoreDuringBuilds: true },
 	generateEtags: true,
 	reactProductionProfiling: true,
+	experimental: {
+		typedRoutes: true,
+	},
 };
 
 export default nextConfig;
